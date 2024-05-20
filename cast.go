25ebd5dbc6c6ec2b8bc4584b9ef47b36c019a345
Dmitrii Aleksandrov <goodmobiledevices@gmail.com>
2024-05-20 14:26:01 +0300

    refactoring and tests
    Signed-off-by: Dmitrii Aleksandrov <goodmobiledevices@gmail.com>

--- cast.go
+++ cast.go
@@ -5,152 +5,46 @@ import (
 	"reflect"
 	"strconv"
 	"time"
-	"unsafe"
 
 	"github.com/google/uuid"
 )
 
-func getBit(val any) int {
-	bit := 0
-
-	switch val.(type) {
-	case int8, uint8, *int8, *uint8:
-		bit = 8
-	case int16, uint16, *int16, *uint16:
-		bit = 16
-	case int, uint, *int, *uint:
-		bit = 0
-	case int32, uint32, float32, *int32, *uint32, *float32:
-		bit = 32
-	case int64, uint64, float64, *int64, *uint64, *float64:
-		bit = 64
-	}
-	return bit
-}
-
-func getNumberType(val any) numberType {
-	t := numberTypeInteger
-	switch val.(type) {
-	case uint, uint8, uint16, uint32, uint64, *uint, *uint8, *uint16, *uint32, *uint64:
-		t = numberTypeUnsignedInteger
-	case float32, float64, *float32, *float64:
-		t = numberTypeFloat
-	}
-	return t
-}
-
-func numberFromString[T any](s string) (T, error) {
-	val := any(*new(T))
-	isPtr := true
-	switch val.(type) {
-	case *int:
-		val = any(int(0))
-	case *int8:
-		val = any(int8(0))
-	case *int16:
-		val = any(int16(0))
-	case *int32:
-		val = any(int32(0))
-	case *int64:
-		val = any(int64(0))
-	case *uint:
-		val = any(uint(0))
-	case *uint8:
-		val = any(uint8(0))
-	case *uint16:
-		val = any(uint16(0))
-	case *uint32:
-		val = any(uint32(0))
-	case *uint64:
-		val = any(uint64(0))
-	case *float32:
-		val = any(float32(0))
-	case *float64:
-		val = any(float64(0))
-	default:
-		isPtr = false
-	}
-	bit := getBit(val)
-	switch getNumberType(val) {
-	case numberTypeFloat:
-		intVal, err := strconv.ParseFloat(s, bit)
-		if isPtr {
-			ptr := &intVal
-			return *(*T)(unsafe.Pointer(&ptr)), err
-		}
-		return *(*T)(unsafe.Pointer(&intVal)), err
-	case numberTypeInteger:
-		intVal, err := strconv.ParseInt(s, 10, bit)
-		if isPtr {
-			ptr := &intVal
-			return *(*T)(unsafe.Pointer(&ptr)), err
-		}
-		return *(*T)(unsafe.Pointer(&intVal)), err
-	case numberTypeUnsignedInteger:
-		intVal, err := strconv.ParseUint(s, 10, bit)
-		if isPtr {
-			ptr := &intVal
-			return *(*T)(unsafe.Pointer(&ptr)), err
-		}
-		return *(*T)(unsafe.Pointer(&intVal)), err
-	}
-	return val.(T), nil
-}
-
-func ptrNumberFromString[T any](s string) (T, error) {
-	val, err := numberFromString[T](s)
-	if err != nil {
-		return *new(T), err
-	}
-	return val, nil
-}
-
-func To[T supported](s string) (T, error) {
-	val := any(*new(T))
-	var err error
-	switch val.(type) {
-	case int, int8, int16, int32, int64,
-		uint, uint8, uint16, uint32, uint64,
-		float32, float64:
-		val, err = numberFromString[T](s)
-	case *int, *int8, *int16, *int32, *int64,
-		*uint, *uint8, *uint16, *uint32, *uint64,
-		*float32, *float64:
-		val, err = ptrNumberFromString[T](s)
-	case bool:
-		val, err = strconv.ParseBool(s)
-	case *bool:
-		val, err = strconv.ParseBool(s)
-		if err != nil {
-			val = nil
-		}
-	case string:
-		val = s
-	case *string:
-		val = &s
-	default:
-		return val.(T), fmt.Errorf("cast: unsupported type: %s", val)
-	}
-	return val.(T), err
-}
+var (
+	typeOfPointerInt     = reflect.PointerTo(reflect.TypeOf(0))
+	typeOfPointerInt8    = reflect.PointerTo(reflect.TypeOf(int8(0)))
+	typeOfPointerInt16   = reflect.PointerTo(reflect.TypeOf(int16(0)))
+	typeOfPointerInt32   = reflect.PointerTo(reflect.TypeOf(int32(0)))
+	typeOfPointerInt64   = reflect.PointerTo(reflect.TypeOf(int64(0)))
+	typeOfPointerUint    = reflect.PointerTo(reflect.TypeOf(uint(0)))
+	typeOfPointerUint8   = reflect.PointerTo(reflect.TypeOf(uint8(0)))
+	typeOfPointerUint16  = reflect.PointerTo(reflect.TypeOf(uint16(0)))
+	typeOfPointerUint32  = reflect.PointerTo(reflect.TypeOf(uint32(0)))
+	typeOfPointerUint64  = reflect.PointerTo(reflect.TypeOf(uint64(0)))
+	typeOfPointerFloat32 = reflect.PointerTo(reflect.TypeOf(float32(0)))
+	typeOfPointerFloat64 = reflect.PointerTo(reflect.TypeOf(float64(0)))
+	typeOfPointerBool    = reflect.PointerTo(reflect.TypeOf(false))
+	typeOfPointerString  = reflect.PointerTo(reflect.TypeOf(""))
+	typeOfPointerTime    = reflect.PointerTo(reflect.TypeOf(time.Time{}))
+	typeOfPointerUUID    = reflect.PointerTo(reflect.TypeOf(uuid.UUID{}))
+)
 
 var mapFunc = map[reflect.Type]func(string, any) error{
-	reflect.PointerTo(reflect.TypeOf(0)):           fromToInt,
-	reflect.PointerTo(reflect.TypeOf(int8(0))):     fromToInt,
-	reflect.PointerTo(reflect.TypeOf(int16(0))):    fromToInt,
-	reflect.PointerTo(reflect.TypeOf(int32(0))):    fromToInt,
-	reflect.PointerTo(reflect.TypeOf(int64(0))):    fromToInt,
-	reflect.PointerTo(reflect.TypeOf(uint(0))):     fromToUint,
-	reflect.PointerTo(reflect.TypeOf(uint8(0))):    fromToUint,
-	reflect.PointerTo(reflect.TypeOf(uint16(0))):   fromToUint,
-	reflect.PointerTo(reflect.TypeOf(uint32(0))):   fromToUint,
-	reflect.PointerTo(reflect.TypeOf(uint64(0))):   fromToUint,
-	reflect.PointerTo(reflect.TypeOf(float32(0))):  fromToFloat,
-	reflect.PointerTo(reflect.TypeOf(float64(0))):  fromToFloat,
-	reflect.PointerTo(reflect.TypeOf(true)):        fromToFloat,
-	reflect.PointerTo(reflect.TypeOf("")):          fromToString,
-	reflect.PointerTo(reflect.TypeOf(time.Now())):  fromToTime,
-	reflect.PointerTo(reflect.TypeOf(uuid.UUID{})): fromToUUID,
+	typeOfPointerInt:     fromToInt,
+	typeOfPointerInt8:    fromToInt,
+	typeOfPointerInt16:   fromToInt,
+	typeOfPointerInt32:   fromToInt,
+	typeOfPointerInt64:   fromToInt,
+	typeOfPointerUint:    fromToUint,
+	typeOfPointerUint8:   fromToUint,
+	typeOfPointerUint16:  fromToUint,
+	typeOfPointerUint32:  fromToUint,
+	typeOfPointerUint64:  fromToUint,
+	typeOfPointerFloat32: fromToFloat,
+	typeOfPointerFloat64: fromToFloat,
+	typeOfPointerBool:    fromToBool,
+	typeOfPointerString:  fromToString,
+	typeOfPointerTime:    fromToTime,
+	typeOfPointerUUID:    fromToUUID,
 }
 
 func fromToInt(s string, to any) error {
@@ -243,40 +137,28 @@ func fromToUUID(s string, to any) error {
 	return nil
 }
 
-func FromTo(s string, to any) error {
-	switch ptr := to.(type) {
-	case *time.Time:
-		return fromToTime(s, ptr)
-	case *int, *int8, *int16, *int32, *int64:
-		return fromToInt(s, ptr)
-	case *uint, *uint8, *uint16, *uint32, *uint64:
-		return fromToUint(s, ptr)
-	case *float64, *float32:
-		return fromToFloat(s, ptr)
-	case *bool:
-		return fromToBool(s, ptr)
-	case *string:
-		return fromToString(s, ptr)
-	case *uuid.UUID:
-		return fromToUUID(s, ptr)
+func ToFrom(s string, to any) error {
+	typeOfPtr := reflect.TypeOf(to)
+	typeOfVal := typeOfPtr.Elem()
+	val, err := ToReflect(s, typeOfVal)
+	if err != nil {
+		return err
 	}
+	reflect.ValueOf(to).Elem().Set(reflect.ValueOf(val))
 	return nil
 }
 
-func GenericTo[T supported](s string) (T, error) {
-	var to any = new(T)
+func To[T supported](s string) (T, error) {
+	to := *new(T)
 	toTypeOf := reflect.TypeOf(to)
-	if toTypeOf.Kind() == reflect.Ptr && toTypeOf.Elem().Kind() == reflect.Ptr {
-		to = *new(T)
+	val, err := ToReflect(s, toTypeOf)
+	if err != nil {
+		return to, err
 	}
-	err := FromTo(s, to)
-	if toTypeOf.Kind() == reflect.Ptr && toTypeOf.Elem().Kind() == reflect.Ptr {
-		return to.(T), err
-	}
-	return to.(T), err
+	return val.(T), nil
 }
 
-func ReflectTo(s string, toType reflect.Type) (any, error) {
+func ToReflect(s string, toType reflect.Type) (any, error) {
 	pointerWrap := 0
 	for toType.Kind() == reflect.Ptr {
 		pointerWrap++
---
+++ cast_test.go
@@ -0,0 +1,336 @@
+package cast
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTo(t *testing.T) {
+	assert := assert.New(t)
+
+	// Testing integers
+	valInt, err := To[int]("56")
+	assert.NoError(err)
+	assert.Equal(56, valInt)
+
+	// Testing pointers to integers
+	valIntPtr, err := To[*int]("42")
+	assert.NoError(err)
+	assert.Equal(42, *valIntPtr)
+
+	// Testing booleans
+	valBool, err := To[bool]("true")
+	assert.NoError(err)
+	assert.Equal(true, valBool)
+
+	// Testing pointers to booleans
+	valBoolPtr, err := To[*bool]("true")
+	assert.NoError(err)
+	assert.Equal(true, *valBoolPtr)
+
+	// Testing strings
+	valString, err := To[string]("hello")
+	assert.NoError(err)
+	assert.Equal("hello", valString)
+
+	// Testing pointers to strings
+	valStringPtr, err := To[*string]("world")
+	assert.NoError(err)
+	assert.Equal("world", *valStringPtr)
+
+	// Testing integers of different bits
+	valInt8, err := To[int8]("12")
+	assert.NoError(err)
+	assert.Equal(int8(12), valInt8)
+
+	valInt16, err := To[int16]("1234")
+	assert.NoError(err)
+	assert.Equal(int16(1234), valInt16)
+
+	valInt32, err := To[int32]("12345")
+	assert.NoError(err)
+	assert.Equal(int32(12345), valInt32)
+
+	valInt64, err := To[int64]("123456789")
+	assert.NoError(err)
+	assert.Equal(int64(123456789), valInt64)
+
+	// Testing pointers to integers of different bits
+	valIntPtr8, err := To[*int8]("42")
+	assert.NoError(err)
+	assert.Equal(int8(42), *valIntPtr8)
+
+	valIntPtr16, err := To[*int16]("1234")
+	assert.NoError(err)
+	assert.Equal(int16(1234), *valIntPtr16)
+
+	valIntPtr32, err := To[*int32]("12345")
+	assert.NoError(err)
+	assert.Equal(int32(12345), *valIntPtr32)
+
+	valIntPtr64, err := To[*int64]("123456789")
+	assert.NoError(err)
+	assert.Equal(int64(123456789), *valIntPtr64)
+
+	// Testing unsigned integers of different bits
+	valUint8, err := To[uint8]("12")
+	assert.NoError(err)
+	assert.Equal(uint8(12), valUint8)
+
+	valUint16, err := To[uint16]("1234")
+	assert.NoError(err)
+	assert.Equal(uint16(1234), valUint16)
+
+	valUint32, err := To[uint32]("12345")
+	assert.NoError(err)
+	assert.Equal(uint32(12345), valUint32)
+
+	valUint64, err := To[uint64]("123456789")
+	assert.NoError(err)
+	assert.Equal(uint64(123456789), valUint64)
+
+	// Testing pointers to unsigned integers of different bits
+	valUintPtr8, err := To[*uint8]("42")
+	assert.NoError(err)
+	assert.Equal(uint8(42), *valUintPtr8)
+
+	valUintPtr16, err := To[*uint16]("1234")
+	assert.NoError(err)
+	assert.Equal(uint16(1234), *valUintPtr16)
+
+	valUintPtr32, err := To[*uint32]("12345")
+	assert.NoError(err)
+	assert.Equal(uint32(12345), *valUintPtr32)
+
+	valUintPtr64, err := To[*uint64]("123456789")
+	assert.NoError(err)
+	assert.Equal(uint64(123456789), *valUintPtr64)
+
+	// Testing floats of different bits
+	valFloat32, err := To[float32]("123.45")
+	assert.NoError(err)
+	assert.Equal(float32(123.45), valFloat32)
+
+	valFloat64, err := To[float64]("123.456789")
+	assert.NoError(err)
+	assert.Equal(float64(123.456789), valFloat64)
+
+	// Testing pointers to integers of floats
+	valFloatPtr32, err := To[*float32]("42.42")
+	assert.NoError(err)
+	assert.Equal(float32(42.42), *valFloatPtr32)
+
+	valFloatPtr64, err := To[*float64]("123.456789")
+	assert.NoError(err)
+	assert.Equal(float64(123.456789), *valFloatPtr64)
+}
+
+func TestFromTo(t *testing.T) {
+	assert := assert.New(t)
+
+	// Define the variable for ToFrom function
+	var (
+		valInt     int
+		valBool    bool
+		valString  string
+		valInt8    int8
+		valInt16   int16
+		valInt32   int32
+		valInt64   int64
+		valUint8   uint8
+		valUint16  uint16
+		valUint32  uint32
+		valUint64  uint64
+		valFloat32 float32
+		valFloat64 float64
+	)
+
+	// Assume that ToFrom method accepts two arguments: a string and a pointer to set the converted value.
+	// Testing integers
+	assert.NoError(ToFrom("56", &valInt))
+	assert.Equal(56, valInt)
+
+	// Testing booleans
+	assert.NoError(ToFrom("true", &valBool))
+	assert.Equal(true, valBool)
+
+	// Testing strings
+	assert.NoError(ToFrom("hello", &valString))
+	assert.Equal("hello", valString)
+
+	// Testing integers of different bits
+	assert.NoError(ToFrom("12", &valInt8))
+	assert.Equal(int8(12), valInt8)
+
+	assert.NoError(ToFrom("1234", &valInt16))
+	assert.Equal(int16(1234), valInt16)
+
+	assert.NoError(ToFrom("12345", &valInt32))
+	assert.Equal(int32(12345), valInt32)
+
+	assert.NoError(ToFrom("123456789", &valInt64))
+	assert.Equal(int64(123456789), valInt64)
+
+	// Testing unsigned integers of different bits
+	assert.NoError(ToFrom("12", &valUint8))
+	assert.Equal(uint8(12), valUint8)
+
+	assert.NoError(ToFrom("1234", &valUint16))
+	assert.Equal(uint16(1234), valUint16)
+
+	assert.NoError(ToFrom("12345", &valUint32))
+	assert.Equal(uint32(12345), valUint32)
+
+	assert.NoError(ToFrom("123456789", &valUint64))
+	assert.Equal(uint64(123456789), valUint64)
+
+	// Testing floats of different bits
+	assert.NoError(ToFrom("123.45", &valFloat32))
+	assert.Equal(float32(123.45), valFloat32)
+
+	assert.NoError(ToFrom("123.456789", &valFloat64))
+	assert.Equal(float64(123.456789), valFloat64)
+}
+
+func TestReflectTo(t *testing.T) {
+	assert := assert.New(t)
+
+	// Define the variables for ToReflect function
+	var (
+		valInt        int
+		valIntPtr     *int
+		valBool       bool
+		valBoolPtr    *bool
+		valString     string
+		valStringPtr  *string
+		valInt8       int8
+		valInt16      int16
+		valInt32      int32
+		valInt64      int64
+		valIntPtr8    *int8
+		valIntPtr16   *int16
+		valIntPtr32   *int32
+		valIntPtr64   *int64
+		valUint8      uint8
+		valUint16     uint16
+		valUint32     uint32
+		valUint64     uint64
+		valUintPtr8   *uint8
+		valUintPtr16  *uint16
+		valUintPtr32  *uint32
+		valUintPtr64  *uint64
+		valFloat32    float32
+		valFloat64    float64
+		valFloatPtr32 *float32
+		valFloatPtr64 *float64
+	)
+
+	// Testing integers
+	res, err := ToReflect("56", reflect.TypeOf(valInt))
+	assert.NoError(err)
+	assert.Equal(56, res.(int))
+
+	// Continue testing other types
+	res, err = ToReflect("true", reflect.TypeOf(valBool))
+	assert.NoError(err)
+	assert.Equal(true, res.(bool))
+
+	res, err = ToReflect("hello", reflect.TypeOf(valString))
+	assert.NoError(err)
+	assert.Equal("hello", res.(string))
+
+	// And some more tests
+	res, err = ToReflect("1", reflect.TypeOf(valInt8))
+	assert.NoError(err)
+	assert.Equal(int8(1), res.(int8))
+
+	res, err = ToReflect("2", reflect.TypeOf(valInt16))
+	assert.NoError(err)
+	assert.Equal(int16(2), res.(int16))
+
+	res, err = ToReflect("3", reflect.TypeOf(valInt32))
+	assert.NoError(err)
+	assert.Equal(int32(3), res.(int32))
+
+	res, err = ToReflect("4", reflect.TypeOf(valInt64))
+	assert.NoError(err)
+	assert.Equal(int64(4), res.(int64))
+
+	res, err = ToReflect("5", reflect.TypeOf(valUint8))
+	assert.NoError(err)
+	assert.Equal(uint8(5), res.(uint8))
+
+	res, err = ToReflect("6", reflect.TypeOf(valUint16))
+	assert.NoError(err)
+	assert.Equal(uint16(6), res.(uint16))
+
+	res, err = ToReflect("7", reflect.TypeOf(valUint32))
+	assert.NoError(err)
+	assert.Equal(uint32(7), res.(uint32))
+
+	res, err = ToReflect("8", reflect.TypeOf(valUint64))
+	assert.NoError(err)
+	assert.Equal(uint64(8), res.(uint64))
+
+	res, err = ToReflect("0.9", reflect.TypeOf(valFloat32))
+	assert.NoError(err)
+	assert.Equal(float32(0.9), res.(float32))
+
+	res, err = ToReflect("1.0", reflect.TypeOf(valFloat64))
+	assert.NoError(err)
+	assert.Equal(float64(1.0), res.(float64))
+
+	res, err = ToReflect("56", reflect.TypeOf(valIntPtr))
+	assert.NoError(err)
+	assert.Equal(56, *res.(*int))
+
+	res, err = ToReflect("true", reflect.TypeOf(valBoolPtr))
+	assert.NoError(err)
+	assert.Equal(true, *res.(*bool))
+
+	res, err = ToReflect("hello", reflect.TypeOf(valStringPtr))
+	assert.NoError(err)
+	assert.Equal("hello", *res.(*string))
+
+	res, err = ToReflect("1", reflect.TypeOf(valIntPtr8))
+	assert.NoError(err)
+	assert.Equal(int8(1), *res.(*int8))
+
+	res, err = ToReflect("2", reflect.TypeOf(valIntPtr16))
+	assert.NoError(err)
+	assert.Equal(int16(2), *res.(*int16))
+
+	res, err = ToReflect("3", reflect.TypeOf(valIntPtr32))
+	assert.NoError(err)
+	assert.Equal(int32(3), *res.(*int32))
+
+	res, err = ToReflect("4", reflect.TypeOf(valIntPtr64))
+	assert.NoError(err)
+	assert.Equal(int64(4), *res.(*int64))
+
+	res, err = ToReflect("5", reflect.TypeOf(valUintPtr8))
+	assert.NoError(err)
+	assert.Equal(uint8(5), *res.(*uint8))
+
+	res, err = ToReflect("6", reflect.TypeOf(valUintPtr16))
+	assert.NoError(err)
+	assert.Equal(uint16(6), *res.(*uint16))
+
+	res, err = ToReflect("7", reflect.TypeOf(valUintPtr32))
+	assert.NoError(err)
+	assert.Equal(uint32(7), *res.(*uint32))
+
+	res, err = ToReflect("8", reflect.TypeOf(valUintPtr64))
+	assert.NoError(err)
+	assert.Equal(uint64(8), *res.(*uint64))
+
+	res, err = ToReflect("0.9", reflect.TypeOf(valFloatPtr32))
+	assert.NoError(err)
+	assert.Equal(float32(0.9), *res.(*float32))
+
+	res, err = ToReflect("1.0", reflect.TypeOf(valFloatPtr64))
+	assert.NoError(err)
+	assert.Equal(float64(1.0), *res.(*float64))
+}
